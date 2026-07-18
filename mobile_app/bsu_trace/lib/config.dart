b6a4f1d2dd540bf74b33ea9f5207a7c0f7d37102
Alex <alexrefuerzocapute@gmail.com>
2026-07-18 21:43:38 +0800

    Email notif -mobile

--- mobile_app/bsu_trace/lib/config.dart
+++ mobile_app/bsu_trace/lib/config.dart
@@ -1,10 +1,8 @@
 class AppConfig {
   // Set this to true to use the live Render server
-  static const bool isProduction = true; 
-
-  static const String localApiUrl = 'http://192.168.1.18:3000/api';
-  static const String liveApiUrl = 'https://bsu-trace.onrender.com/api'; // <-- Ensure '/api' is at the end!
-
+  static const bool isProduction = false;
+  static const String localApiUrl = 'http://192.168.1.185:3000/api';
+  static const String liveApiUrl =  'https://bsu-trace.onrender.com/api'; // <-- Ensure '/api' is at the end!
   static String get baseUrl {
     return isProduction ? liveApiUrl : localApiUrl;
   }
